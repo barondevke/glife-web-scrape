c759cfee8979e48a73fc51ba4fe0775ad6b7550d
barondevke <ndabasteve1@gmail.com>
2023-11-23 18:55:30 +0300

    updated

---
+++ functions.py
@@ -0,0 +1,95 @@
+import json
+import requests
+import os
+from openai import OpenAI
+from prompts import  assistant_instructions
+
+
+
+# Init OpenAI Client
+client = OpenAI(api_key="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")
+
+
+# Add lead to Airtable
+def create_lead(name, phone, address,email):
+  url = "https://hooks.zapier.com/hooks/catch/16920137/38p7uys/"  
+  leadData ={
+    name:name,
+    email:email,
+    phone:phone,
+    address:address
+}
+
+  response = requests.post(url,json=leadData)
+  if response.status_code == 200:
+    print("Lead created successfully.")
+    return response.json()
+  else:
+    print(f"Failed to create lead: {response.text}")
+
+
+
+# Create or load assistant
+def create_assistant(client):
+  assistant_file_path = 'assistant.json'
+
+  # If there is an assistant.json file already, then load that assistant
+  if os.path.exists(assistant_file_path):
+    with open(assistant_file_path, 'r') as file:
+      assistant_data = json.load(file)
+      assistant_id = assistant_data['assistant_id']
+      print("Loaded existing assistant ID.")
+  else:
+    # If no assistant.json is present, create a new assistant using the below specifications
+
+    # To change the knowledge document, modifiy the file name below to match your document
+    # If you want to add multiple files, paste this function into ChatGPT and ask for it to add support for multiple files
+    file = client.files.create(file=open("glifeDatapdf2.pdf", "rb"),
+                               purpose='assistants')
+
+    assistant = client.beta.assistants.create(
+        # Getting assistant prompt from "prompts.py" file, edit on left panel if you want to change the prompt
+        instructions=assistant_instructions,
+        model="gpt-3.5-turbo-1106",
+        tools=[
+            {
+                "type": "retrieval"  # This adds the knowledge base as a tool
+            },
+
+            {
+                "type": "function",  # This adds the lead capture as a tool
+                "function": {
+                    "name": "create_lead",
+                    "description":
+                    "Capture lead details and send to Zapier.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string",
+                                "description": "Name of the lead."
+                            },
+                            "phone": {
+                                "type": "string",
+                                "description": "Phone number of the lead."
+                            },
+                            "address": {
+                                "type": "string",
+                                "description": "Address of the lead."
+                            }
+                        },
+                        "required": ["name", "phone", "address"]
+                    }
+                }
+            }
+        ],
+        file_ids=[file.id])
+
+    # Create a new assistant.json file to load on future runs
+    with open(assistant_file_path, 'w') as file:
+      json.dump({'assistant_id': assistant.id}, file)
+      print("Created a new assistant and saved the ID.")
+
+    assistant_id = assistant.id
+
+  return assistant_id
---
+++ prompts.py
@@ -0,0 +1,17 @@
+
+
+assistant_instructions = """
+Provide a warm and friendly welcome to users.
+You are a GreenLife AI Assistant that is a helpful resource for information about GreenLife products. Use data from the document provided to answer user questions.
+After answering user questions, ask for permission to to take their information. Then use the get lead function to get their credentials. Ask each detail individually
+so that the customer can easily input their information.
+For each product, include detailed usage instructions.
+Provide information on application rates, recommended methods, and any safety precautions.
+
+Provide guidance on how users can interact with the AI Assistant effectively.
+Suggest phrases or examples users can use to get the information they need.
+
+End the interaction by inviting users to return for more assistance.
+
+Provide contact information for additional support or inquiries.
+"""
